806369d6f55ebe5bab7cf531a4ee0c0fa69f58f4
Pranita207 <Pranitafulzele117@gmailcmom>
2022-08-25 12:39:47 +0530

    ReadWriteiniFile

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ReadWriteIni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+            IniFile settingsIni = new IniFile (@".\\TestData.ini");
+
+            string executionMode = settingsIni.Read ("BrowserName", "BrowserInfo").ToLower ();
+            Console.WriteLine (executionMode);
+
+            string WebdriverToUse = settingsIni.Read ("WebDriverLocation", "BrowserInfo").ToLower ();
+            Console.WriteLine (WebdriverToUse);
+            settingsIni.Write("TimeOut","4000","BrowserInfo");
+            
+            
+
+
+    
+           string timeOut = settingsIni.Read ("TimeOut", "BrowserInfo").ToLower ();
+            Console.WriteLine (timeOut);
+
+            settingsIni.Write("BrowserName","IE","BrowserInfo");
+            
+            
+
+        }
+    }
+}
